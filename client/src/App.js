1ef4ddd7dc0afc0ccd085d9b91d3a816064bfc5f
Rob Martin <rob.martin.5758@gmail.com>
2018-07-21 20:24:12 -0400

    structure changes

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
-import React, { Component } from 'react';
-import './App.css';
-import LinkComp from "./components/stateless/link/link"
+import React, { Component } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { FolderPage } from './components/folder/folder';
+// import { Link } from './components/link';
+// import { HomePage } from './components/home';
 
 class App extends Component {
   render() {
     return (
-      <div>
-      <LinkComp></LinkComp>
-      </div>
+      <Router>
+        <Switch>
+          <Route exact path="/" component={FolderPage} />
+        </Switch>
+      </Router>
     );
   }
 }
